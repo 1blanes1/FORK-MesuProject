c35f03154f8a2340f6a2b6fe195c79ffe6fa519d
V4n1sh795 <v4n1shnerush@gmail.com>
2025-10-04 21:48:41 +0500

    -

--- json_api/api/functions.py
+++ json_api/api/functions.py
@@ -34,7 +34,7 @@ def add_item_to_json(new_item, filename='news.json'):
     data.append(new_item)
 
     # Сохраняем обратно в файл
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f: 
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 async def get_item_by_id(target_id):
