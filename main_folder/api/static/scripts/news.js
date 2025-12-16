92e3441db2e0b47ef2b40426dee0578f07b5e8e3
egorsubbotin <egor.a.subbotin@mail.ru>
2025-12-16 19:43:21 +0500

    fixed description at news

--- main_folder/api/static/scripts/news.js
+++ main_folder/api/static/scripts/news.js
@@ -45,7 +45,7 @@ function renderNewsPage(page) {
         // Описание
         const newDesc = document.createElement('p');
         newDesc.className = 'news-desc';
-        newDesc.textContent = news.description || news.title || 'Без описания';
+        newDesc.textContent = news.desc || 'Без описания';
 
         // Сборка
         newsLink.appendChild(newsTitle);
